183daf9463df4c043ae98b14e335e500388ad5e7
MasoodSaamer <masoodofficial27@gmail.com>
2024-08-17 23:49:02 +0900

    Updated site icon and Navbar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className="flex items-center flex-shrink-0">
                 {/*This part is for the Logo and title that will display at the top left of the webpage */}
                 <img className="h-10 w-10 mr-2" src={logo} alt="Studio Logo" />
-                <span className="text-xl tracking-tight">Protocol Studio</span> 
+                <span className="text-xl tracking-tight">Protocol Simulation Studio</span> 
             </div>
             {/*This part is for the quick link navigation bar at the top of the webpage */}
             <ul className="hidden lg:flex ml-14 space-x-12">
